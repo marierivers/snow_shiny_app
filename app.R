c61a85b4c12b1ce32ab005959d524e229c653a38
marierivers <mrivers@bren.ucsb.edu>
2022-03-06 00:32:59 -0800

    inital commit

---
+++ app.R
@@ -0,0 +1,53 @@
+# a shiny app for snow data
+
+# attach packages
+library(shiny)
+library(tidyverse)
+library(paletteer)
+
+# load data
+california <- read_csv("california_snow_data.csv")
+
+# create 'ui' = "User Interface"
+# widgets are things that the user interacts with to make decisions about what they want to appear as outputs
+ui <- fluidPage(
+  titlePanel("California Snow Data"),
+  sidebarLayout(
+    # sidebarPanel is where you put your widgets
+    sidebarPanel("here are my widgets: user selection options", 
+                 radioButtons(inputId = "site", 
+                              label = "chose a site:",
+                              choices = c("bunkerhill", "pliocenrdg", "babbittpk" = "babbittpk_", "martispeak", "schoolhous", "buzzardrst", "mazourkapk", "mt gleason" = "mt_gleason", "grapevine" = "grapevine_", "chileoflat", "laurel creek" = "laurel_crk", "pumicevall", "mono mills" = "mono_mills", "sagehenmdw", "owensgorge", "fossiltilt", "cratermark", "granitemtn", "blindsprin", "conwayroad", "lvmanzlake", "ballardrdg"))),
+    mainPanel("here is my graph", 
+              plotOutput(outputId = "snow_plot"))
+  )
+)
+
+# create 'server'
+# the server is a function that takes in inputs which are going to be the things that the user selects and then it's going to send back outputs which the user can see.
+server <- function(input, output) {
+  
+  # created a reactive dataframe that depends on the selection made in the site widget
+  site_select <- reactive({
+    california %>% 
+      filter(site_name == input$site)})
+  
+  output$snow_plot <- renderPlot({
+    
+    # if the data that you are using is a reactive dataframe then you have to add () after data = df()
+    ggplot(data = site_select(), aes(x = day_of_wy, y = running_wy_snow_accumulation_m)) +
+      geom_line(aes(color = as.factor(water_year)), size = 1) +
+      scale_x_continuous(breaks = c(1, 32, 62, 93, 124, 152, 183, 213, 244, 275),
+                         labels = c("Oct 01", "Nov 01", "Dec 01", "Jan 01", "Feb 01", "Mar 01", "Apr 01", "May 01", "Jun 01", "Jul 01")) +
+      #scale_colour_paletteer_d("khroma::smooth_rainbow") +
+      theme(legend.position = "bottom") +
+      labs(title = "total snow accumulation per water year", subtitle = input$site,
+           x = NULL, y = "total snow (m)", color = "water year")
+    })
+  
+  
+}
+
+
+# Let R know that you want to combine the ui & server into an app:
+shinyApp(ui = ui, server = server)
