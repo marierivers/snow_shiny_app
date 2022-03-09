0ba93ec2189e88b8022f43e3a25e91cbb9b7f404
marierivers <mrivers@bren.ucsb.edu>
2022-03-08 23:43:26 -0800

    added bar chart

--- app.R
+++ app.R
@@ -8,25 +8,33 @@ library(shinythemes)
 library(leaflet)
 library(DT)
 
+
 # load data
 california <- read_csv("california_snow_data.csv")
 ca_stations <- read_csv("ca_stations.csv")
+#saveRDS(temp_month_summary, here::here(".", "myapp", "data", "temp_month_summary.rds"))
+#temp_summary <- readRDS("data/temp_month_summary.rds")
 
 # create 'ui' = "User Interface"
 # widgets are things that the user interacts with to make decisions about what they want to appear as outputs
 ui <- fluidPage(
   navbarPage("California Snow Data",
+             # theme = bslib::bs_theme(
+             #   bg = "#101010", # background color
+             #   fg = "#FDF7F7", # foreground color
+             #   primary = "#ED79F9"), # primary accent color
+             #theme = bslib::bs_theme(bootswatch = "minty"),
              #theme = "theme_mtr.css",
              theme = shinytheme("cerulean"),
              #theme = shinytheme("cyborg"),
-                 tabPanel("first tab",
-                          h1("some giant text"),
-                          p("use this map to find the name of a site near your water supply"),
+                 tabPanel("site locations",
+                          h1("Daily Snow Depth Stations"),
+                          p("This map shows California sites that contain daily snow depths estimated from GPS signal-to-noise ratios (SNRs). This dataset allows water manager to better understand the quantity of winter snowpacks. Use this map to find the name of a site near your water supply."),
                           leafletOutput(outputId = "site_map1")),
-                 tabPanel("second tab",
+                 tabPanel("snow graphs",
                           sidebarLayout(
                             # sidebarPanel is where you put your widgets
-                            sidebarPanel("here are my widgets: user selection options", 
+                            sidebarPanel("user selection options", 
                                          radioButtons(inputId = "site", 
                                                       label = "chose a site:",
                                                       choices = c("bunkerhill", "pliocenrdg", "babbittpk" = "babbittpk_", "martispeak", "schoolhous", "buzzardrst", "mazourkapk", "mt gleason" = "mt_gleason", "grapevine" = "grapevine_", "chileoflat", "laurel creek" = "laurel_crk", "pumicevall", "mono mills" = "mono_mills", "sagehenmdw", "owensgorge", "fossiltilt", "cratermark", "granitemtn", "blindsprin", "conwayroad", "lvmanzlake", "ballardrdg")),
@@ -42,15 +50,18 @@ ui <- fluidPage(
                                       leafletOutput(outputId = "site_map2"),
                                       "here is my graph",
                                       tabsetPanel(
-                                        tabPanel("running total", plotOutput(outputId = "snow_plot")),
-                                        tabPanel("mix/max"),
-                                        tabPanel("snow depth", plotOutput(outputId = "depth_plot"))))
+                                        tabPanel("snow depth: running total", plotOutput(outputId = "snow_plot")),
+                                        #tabPanel("mix/max"),
+                                        tabPanel("snow depth: daily fluctuation", plotOutput(outputId = "depth_plot")),
+                                        tabPanel("snow depth: annual total", plotOutput(outputId = "wy_total_bar_chart"))),
+                                      p("Citation: Larson, K. M. and E. E. Small. 2017. Daily Snow Depth and SWE from GPS Signal-to-Noise Ratios, Version 1. [Indicate subset used]. Boulder, Colorado USA. NASA National Snow and Ice Data Center Distributed Active Archive Center. doi: https://doi.org/10.5067/Z02Y1HGNFXCH. [Date Accessed]."))
                           )
                           ),
-                 tabPanel("the data",
+                 tabPanel("data table",
                           p("something with a date input or date range slider" ),
                           # data table
-                          DT::dataTableOutput(outputId = "california_data"))
+                          DT::dataTableOutput(outputId = "california_data"),
+                          p("Citation: Larson, K. M. and E. E. Small. 2017. Daily Snow Depth and SWE from GPS Signal-to-Noise Ratios, Version 1. [Indicate subset used]. Boulder, Colorado USA. NASA National Snow and Ice Data Center Distributed Active Archive Center. doi: https://doi.org/10.5067/Z02Y1HGNFXCH. [Date Accessed]."))
   
 ))
 
@@ -68,10 +79,19 @@ server <- function(input, output) {
       filter(water_year %in% c(input$wy_select))
     })
   
+  # create a reactive dataframe of annual snow totals based on selected site
+  annual_totals <- reactive({
+    california %>% 
+      filter(site_name == input$site) %>% 
+      group_by(water_year) %>% 
+      summarise(total_snow_wy_m = max(running_wy_snow_accumulation_m)) %>% 
+      filter(water_year %in% c(input$wy_select))
+  })
+
+  # running total of accumulated snow
   output$snow_plot <- renderPlot({
-    
-    # if the data that you are using is a reactive dataframe then you have to add () after data = df()
-    ggplot(data = site_select(), aes(x = day_of_wy, y = running_wy_snow_accumulation_m)) +
+      # if the data that you are using is a reactive dataframe then you have to add () after data = df()
+        ggplot(data = site_select(), aes(x = day_of_wy, y = running_wy_snow_accumulation_m)) +
       geom_line(aes(color = as.factor(water_year)), size = 1) +
       scale_x_continuous(breaks = c(1, 32, 62, 93, 124, 152, 183, 213, 244, 275),
                          labels = c("Oct 01", "Nov 01", "Dec 01", "Jan 01", "Feb 01", "Mar 01", "Apr 01", "May 01", "Jun 01", "Jul 01")) +
@@ -81,6 +101,7 @@ server <- function(input, output) {
            x = NULL, y = "total snow (m)", color = "water year")
     })
   
+  # daily change in snow depth
   output$depth_plot <- renderPlot({
     # if the data that you are using is a reactive dataframe then you have to add () after data = df()
     ggplot(data = site_select(), aes(x = day_of_wy, y = snow_depth_m)) +
@@ -93,21 +114,34 @@ server <- function(input, output) {
            x = NULL, y = "total snow (m)", color = "water year")
   })
   
+  # bar chart of total snow per water year
+  output$wy_total_bar_chart <- renderPlot({
+    ggplot(data = annual_totals(), aes(x = water_year, y = total_snow_wy_m)) +
+      geom_col(fill = "blue") +
+      labs(title = "Total accumulated snow per water year",
+           x = "water year", y = "total snow depth (m)")
+  })
+  
+  
+  # map of all sites
   output$site_map1 <- renderLeaflet({
     leaflet() %>% 
       addProviderTiles(providers$Stamen.TonerLite,
                        options = providerTileOptions(noWrap = TRUE)
                        ) %>% 
-      addMarkers(data = ca_stations, lat = ~latitude, lng = ~longitude, popup = ~site_name)
+      addMarkers(data = ca_stations, lat = ~latitude, lng = ~longitude, label = ~site_name)
   })
   
+  # map of just selected site
   output$site_map2 <- renderLeaflet({
-    leaflet(options = leafletOptions(minZoom = 5, maxZoom = 20)) %>% 
+    leaflet() %>% 
+      setView(lat = 38, lng = -120, zoom = 5.25) %>% 
       addProviderTiles(providers$Stamen.TonerLite,
-                       options = providerTileOptions(noWrap = TRUE, minZoom = 5, maxZoom = 20)) %>% 
-      addMarkers(data = site_select(), lat = ~latitude, lng = ~longitude, popup = ~site_name, 
-                 options = markerOptions(minZoom = 5, maxZoom = 20))
+                       options = providerTileOptions(noWrap = TRUE)) %>% 
+      addMarkers(data = site_select(), lat = ~latitude, lng = ~longitude, popup = ~site_name)
+    #addMarkers(data = site_select(), lat = ~latitude, lng = ~longitude, popup = ~site_name, options = markerOptions(minZoom = 5, maxZoom = 20))
   })
+  
   
   # render the california data table
   output$california_data <- DT::renderDataTable({
